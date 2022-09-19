1977006acc811ec08c8c8d1a625ad5f3327b01d4
akinyosoyeisaac <akinyosoyeisaac@gmail.com>
2022-09-19 22:30:39 +0100

    working on improve the accuracy using a finetune model

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from argparse import ArgumentParser
 from logs import get_logger
 from preprocessing import train_images, val_images
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger, TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger, TensorBoard, LearningRateScheduler
 from tensorflow import keras
 
 
@@ -32,7 +32,7 @@ pooling = config["training"]["pretrain_p"]
 )
 
 # Freeze weights
-pretrained_model.trainable = False
+pretrained_model.trainable = True
 
 # Create weights
 inputs = pretrained_model.input
@@ -52,15 +52,33 @@ model.compile(
 
 logger.info(model.summary())
 
+LR_START = 0.00001
+LR_MAX = 0.0001 * 8
+LR_MIN = 0.00001
+LR_RAMPUP_EPOCHS = 3
+LR_SUSTAIN_EPOCHS = 3
+LR_EXP_DECAY = .5
+
+def lrfn(epoch):
+    if epoch < LR_RAMPUP_EPOCHS:
+        lr = (LR_MAX - LR_START) / LR_RAMPUP_EPOCHS * epoch + LR_START
+    elif epoch < LR_RAMPUP_EPOCHS + LR_SUSTAIN_EPOCHS:
+        lr = LR_MAX
+    else:
+        lr = (LR_MAX - LR_MIN) * LR_EXP_DECAY**(epoch - LR_RAMPUP_EPOCHS - LR_SUSTAIN_EPOCHS) + LR_MIN
+    return lr
+    
+lr_callback = LearningRateScheduler(lrfn, verbose=True)
+
 earlystop = EarlyStopping(
             monitor = 'val_loss',
-            patience = 2,
+            patience = 5,
             restore_best_weights = True)  
 model_names = "model/checkpoint/model.{epoch:02d}-{val_accuracy:.2f}.hdf5" 
 checkpoint = ModelCheckpoint(model_names, monitor="val_accuracy", verbose=1, save_best_only=True, save_weights_only=False)
 log_file_path = "model/log.txt"
 csv_logger = CSVLogger(log_file_path, append=False)
-tensorboard_log_dir = "model/logs_" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_log_dir = "model/logs_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 tensorboard = TensorBoard(log_dir=tensorboard_log_dir, histogram_freq=1)
 
 
@@ -71,7 +89,7 @@ history = model.fit(
     validation_data = val_images,
     batch_size = 32,
     epochs = 100,
-    callbacks = [earlystop, csv_logger, checkpoint, tensorboard]
+    callbacks = [lr_callback, earlystop, csv_logger, checkpoint, tensorboard]
 )
 
 
